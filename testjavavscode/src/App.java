b219ccc8d902da5b30bf11caab6c672ec9273182
Cushman <oliver.cushman@mrjaffesclass.com>
2022-10-13 21:01:26 -0700

    added doc
    added comments

--- testjavavscode/src/App.java
+++ testjavavscode/src/App.java
@@ -4,14 +4,14 @@ public class App {
         double k;
         
         for (int i = 1; i <= 100; i++) {
-            j = (double) i / 3;
-            k = (double) i / 5;
+            j = (double) i / 3; // stores i / 3 as a double in j
+            k = (double) i / 5; // stores i / 5 as a double in k
             
-            if (j == (int) j && k == (int) k) {
+            if (j == (int) j && k == (int) k) { // checks if j and k are divisible by 3 and 5
                 System.out.println("FizzBuzz");
-            } else if (j == (int) j) {
+            } else if (j == (int) j) { // checks if j is divisible by 3
                 System.out.println("Fizz");
-            } else if (k == (int) k) {
+            } else if (k == (int) k) { // checks if k is divisible by 5
                 System.out.println("Buzz");
             } else {
                 System.out.println(i);
